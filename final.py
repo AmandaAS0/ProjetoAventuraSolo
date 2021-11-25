de3fa38c711f5a802903e82f4eb4896447844457
Amanda Aguiar <82977456+AmandaAS0@users.noreply.github.com>
2021-11-25 01:23:55 -0300

    Alterei o final da história do ladino.

--- final.py
+++ final.py
@@ -22,11 +22,13 @@ def final_maga():
         if final.upper() == "1":
             time.sleep(2)
             print("\n\nSua consciência com o futuro fala mais alto e você pensa que ao destruir esta flor você poderá dar a outras crianças órfãs um futuro melhor. Não apenas de aprender as artes da magia, mas também de aprender o que é ser cuidado, o que é ter uma família e o que é ter amor. Não levar a flor para ele é certamente a melhor coisa a se fazer. \n  Não tarda para que você esteja aos pés do leito de morte do seu instrutor onde um misto de peso e alívio permeia sua consciência. Esta morte você carregará para sempre em suas mãos para que outras dezenas de vidas tenham chances melhores.")
+            print("\n\nEssa aventura encerra por aqui...")
             condicao = False
 
         elif final.upper() == "2":
             time.sleep(2)
             print("Ao tocar na flor pensamentos vem a sua mente, um flash de inúmeras imagens quase como uma lembrança do futuro. \n Ao mantê-lo vivo você terá a oportunidade de aprender cada vez mais tendo a chance de se superar e superar até mesmo aquele que a educou. O poder adquirido será sua maior recompensa por esta escolha.")
+            print("\n\nEssa aventura encerra por aqui...")
             condicao = False
 
         else:
@@ -53,13 +55,15 @@ def final_ladino():
         condicao = True
         if final.upper() == "1":
             time.sleep(1)
-            print("FINAL 1.")
+            print("Você não precisa e não quer voltar pra casa, e menos ainda pra guilda já que agora certamente você terá que fazer parte dela devido a tudo que você sabe. Sua melhor opção é destruir a flor e fugir para muito longe, onde ninguém possa encontrá-lo..")
+            print("\n\nEssa aventura encerra por aqui...")
             condicao = False
 
 
         elif final.upper() == "2":
             time.sleep(2)
-            print("FINAL 2")
+            print("\n\nVocê leva a flor de volta pra guilda, a maldição do item que seu pai carrega e desfeita e você percebe que não consegue mais olhar ele nos olhos. Você não confia mais nele. O melhor a fazer é partir pra outro lugar e tentar construir outra vida")
+            print("\n\nEssa aventura encerra por aqui...") 
             condicao = False
             break
         else:
@@ -86,11 +90,13 @@ def final_guerreira():
         if final.upper() == "1":
             time.sleep(1)
             print("Sua consciência com o todo fala mais alto e você se pergunta? Qual é o custo de uma única vida? Manter sua mãe viva certamente implicará na miséria, morte, colapso do reino e o completo caos pra milhares de pessoas. Ela acreditará na sua nobreza e lealdade e jamais pensará que você está mentindo. Não levar a flor para ela é certamente a melhor coisa a se fazer. \n Não tarda para que você esteja aos pés do leito de morte da sua mãe onde um misto de peso e alívio permeia sua consciência. Esta morte você carregará para toda vida, mas outras milhares serão salvas")
+            print("\n\nEssa aventura encerra por aqui...")
             condicao = False
 
         elif final.upper() == "2":
             time.sleep(2)
             print("Ao tocar na flor pensamentos vem a sua mente, um flash de inúmeras imagens quase como uma lembrança do futuro. \nManter sua mãe viva certamente implicará na miséria, morte, colapso do reino e o completo caos pra milhares de pessoas. Mas onde estaria sua lealdade e sua nobreza se sua missão não fosse cumprida? \nLevar a flor para ela é certamente a coisa certa a se fazer. Sua mãe ganha algo como a vida eterna e com isso o reino mergulha em séculos de pobreza, fome, doenças e incontáveis mortes.")
+            print("\n\nEssa aventura encerra por aqui...")
             condicao = False
 
         else:
